70a524d0d59423baffe8496d4337b6e9437fd4ad
ouyangqiong <aqqwbjgyiy1987@163.com>
2017-07-16 21:20:25 +0800

    add drag-choice click-choice for reading module

--- StudyEnglish/handinhand-ionic2/src/pages/test/reading-test/reading-test.ts
+++ StudyEnglish/handinhand-ionic2/src/pages/test/reading-test/reading-test.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 
 import { NavParams } from 'ionic-angular';
 
-
 import { ConferenceData } from '../../../providers/conference-data';
 
 @Component({
@@ -22,73 +21,99 @@ export class ReadingTestPage {
   total_passage: number = 0;
   datas: any;
   currentPassage: any;
-  dragAnswer: string="";
+  dragAnswer: string = "";
 
   constructor(public navParams: NavParams, public confData: ConferenceData) {
     confData.getReadingTestData("tpo34").subscribe(
-      resulte => 
-            {
-                console.log(resulte)
-                this.passages = resulte
-                this.steps = this.passages["steps"];
-                this.first_step =  this.steps[this.stepindex];
-                this.step = this.first_step;
-                this.last_step =  this.steps[this.passages["steps"].length-1];
-                this.last_stepindex = this.passages["steps"].length-1;
-                this.get_total_graph(this.steps);
-                this.currentPassage = this.passages[this.step]
-            }
+      resulte => {
+        console.log(resulte)
+        this.passages = resulte
+        this.steps = this.passages["steps"];
+        this.first_step = this.steps[this.stepindex];
+        this.step = this.first_step;
+        this.last_step = this.steps[this.passages["steps"].length - 1];
+        this.last_stepindex = this.passages["steps"].length - 1;
+        this.get_total_graph(this.steps);
+        this.currentPassage = this.passages[this.step]
+      }
     );
   }
 
-  private get_total_graph(steps: any[]){
-    for(let step of steps){
-      if(step.indexOf("p")!=-1){
-        this.total_passage=this.total_passage+1
-      }else{
-        this.total_question=this.total_question+1
+  private get_total_graph(steps: any[]) {
+    for (let step of steps) {
+      if (step.indexOf("p") != -1) {
+        this.total_passage = this.total_passage + 1
+      } else {
+        this.total_question = this.total_question + 1
       }
     }
   }
 
-  gotoNext(){
-      if(this.stepindex != this.last_stepindex){
-        this.stepindex = this.stepindex+1
-        this.step =  this.steps[this.stepindex]
-      }
-      if(this.step.indexOf("q")!=-1){
-        let passageStep = this.passages.questions[this.step].passage
-        this.currentPassage = this.passages.passage[passageStep]
-      }else{
-        this.currentPassage = this.passages[this.step]
-      }
+  gotoNext() {
+    if (this.stepindex != this.last_stepindex) {
+      this.stepindex = this.stepindex + 1
+      this.step = this.steps[this.stepindex]
+    }
+    if (this.step.indexOf("q") != -1) {
+      let passageStep = this.passages.questions[this.step].passage
+      this.currentPassage = this.passages.passage[passageStep]
+    } else {
+      this.currentPassage = this.passages[this.step]
+    }
 
   }
-  gotoBack(){
-      if( this.stepindex != 0){
-        this.stepindex = this.stepindex-1
-        this.step =  this.steps[this.stepindex]
-      } 
-      if(this.step.indexOf("q")!=-1){
-        let passageStep = this.passages.questions[this.step].passage
-        this.currentPassage = this.passages.passage[passageStep]
-      }else{
-        this.currentPassage = this.passages.passage[this.step]
-      }
+  gotoBack() {
+    if (this.stepindex != 0) {
+      this.stepindex = this.stepindex - 1
+      this.step = this.steps[this.stepindex]
+    }
+    if (this.step.indexOf("q") != -1) {
+      let passageStep = this.passages.questions[this.step].passage
+      this.currentPassage = this.passages.passage[passageStep]
+    } else {
+      this.currentPassage = this.passages.passage[this.step]
+    }
   }
-  gotoHome(){
-      
+  gotoHome() {
+
   }
 
-  stopTiming(){
-    
+  stopTiming() {
+
   }
-  insertDragContent(event: any){
+
+  insertClickContent(event: any) {
     console.log(event)
     let htmlCollections = document.getElementsByClassName("click-choice")
-    for(let i=0; i<htmlCollections.length;i++){
-      htmlCollections[i].children[0].innerHTML="";
+    for (let i = 0; i < htmlCollections.length; i++) {
+      htmlCollections[i].children[0].innerHTML = "";
     }
-    event.target.children[0].innerHTML=this.passages.questions[this.step].dragContent;
+    event.target.children[0].innerHTML = this.passages.questions[this.step].content;
+  }
+
+  dragChoiceStart(event: any) {
+    event.dataTransfer.setData("Text", event.target.id);
+  }
+
+  drop(event: any) {
+    event.preventDefault();
+    if (event.target.tagName == "DIV" && event.target.children.length == 0) {
+      console.log(event)
+      var data = event.dataTransfer.getData("Text");
+      event.target.appendChild(document.getElementById(data));
+    }
+  }
+
+  allowDrop(event: any) {
+    event.preventDefault();
+  }
+
+  rollbackDrag(event: any) {
+    if (event.target.tagName == "P") {
+      let targetEle = document.getElementById("div"+event.target.id);
+      if(targetEle != null){
+        targetEle.appendChild(event.target)
+      }
+    }
   }
 }
